f62b2bf6f487d20e99cd1eee51fac899929a72aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-13 07:56:14 +0000

    Deploy TuhuFE/share to github.com/TuhuFE/share.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -28,60 +28,60 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.2655e777.js",
-    "revision": "3ace66a4d5b41b478750203f87a7b235"
+    "url": "assets/js/10.77367f05.js",
+    "revision": "d70b3a90ff193798dd596435022a3503"
   },
   {
-    "url": "assets/js/11.1b5b21ec.js",
-    "revision": "9f125bc12e4167c02fc7e339d781f3b4"
+    "url": "assets/js/11.0a8ec6e6.js",
+    "revision": "433c9456ee32cb855b4b0b09ed77f795"
   },
   {
-    "url": "assets/js/12.4cfc540f.js",
-    "revision": "c6af484d2eaa6e3b393f049d0680604f"
+    "url": "assets/js/12.9226fa38.js",
+    "revision": "b4de1b7a7b37bcb7b79a5513d3c2f1e1"
   },
   {
-    "url": "assets/js/13.027d3509.js",
-    "revision": "3c82a1439e58c15aaf5f969127d41aaf"
+    "url": "assets/js/13.8646d975.js",
+    "revision": "739128ba1ea2ba8107f246c3ec1a7a27"
   },
   {
-    "url": "assets/js/2.99d8ca2c.js",
-    "revision": "68b3a2ee1942bb108b2359bad0a00ef6"
+    "url": "assets/js/2.1669a297.js",
+    "revision": "7636b2768fb3e61408647b7153351875"
   },
   {
-    "url": "assets/js/3.0ba4e460.js",
-    "revision": "bb4d6c85e0a5994984562dd653532cf1"
+    "url": "assets/js/3.575a718f.js",
+    "revision": "3200e339eeab1f07221ceecf6befb5c9"
   },
   {
-    "url": "assets/js/4.7674a282.js",
-    "revision": "1fbc82a6377f950f1f7611293c2302ba"
+    "url": "assets/js/4.464fa5b5.js",
+    "revision": "97eda30cb86f55a6450be5af751b92b9"
   },
   {
-    "url": "assets/js/5.6b4b3fc2.js",
-    "revision": "3b2f5e2bd90972009b922a7d62230ae0"
+    "url": "assets/js/5.afc21057.js",
+    "revision": "32b6ad7a350c27f67a1e93479ee65fec"
   },
   {
-    "url": "assets/js/6.4ea24fcf.js",
-    "revision": "26d023515b0831661e10fb19e0334788"
+    "url": "assets/js/6.0b889c3b.js",
+    "revision": "9df930f3d7f5a83d30fd8288008ec27b"
   },
   {
-    "url": "assets/js/7.0b3d3c46.js",
-    "revision": "384a5d4d37bb89914c8b9c44b8244cc9"
+    "url": "assets/js/7.8541c6e9.js",
+    "revision": "460c322949702db6eb7b99fe52306917"
   },
   {
-    "url": "assets/js/8.7eac1576.js",
-    "revision": "c6223cc65154f4fbc7ed88132497bc5c"
+    "url": "assets/js/8.0a84fb0f.js",
+    "revision": "32528baf7be61919c30a4502acb729a1"
   },
   {
-    "url": "assets/js/9.abdaf9be.js",
-    "revision": "866fe6aaec25be3a3b1c4ed044d9c178"
+    "url": "assets/js/9.413abfde.js",
+    "revision": "5c5518c6029ae34ac33fdf2504751e9e"
   },
   {
-    "url": "assets/js/app.a34cb03d.js",
-    "revision": "06e41b97341706573545558c637c8c28"
+    "url": "assets/js/app.5bb9b872.js",
+    "revision": "72ee3b45313776962cc2284cd83595f2"
   },
   {
     "url": "index.html",
-    "revision": "ef52a497fa115cd73a656f6fd12af584"
+    "revision": "06d493fa79ab4f2fc8a88926c5130928"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
